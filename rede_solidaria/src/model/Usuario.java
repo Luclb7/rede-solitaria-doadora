f7d8be7ec064f4ca6ba3869c4dad9046f3a9d808
gilleanmeneses <gilleanmenesesfarias1@gmail.com>
2026-05-21 16:45:14 -0300

    Enhance Usuario constructor with validation
    Added validation for 'nome' and 'email' in the constructor.

--- rede_solidaria/src/model/Usuario.java
+++ rede_solidaria/src/model/Usuario.java
@@ -1,6 +1,7 @@
 package model;
 
 public class Usuario {
+
     protected int id;
     protected String nome;
     protected String telefone;
@@ -8,6 +9,17 @@ public class Usuario {
     protected String endereco;
 
     public Usuario(int id, String nome, String telefone, String email, String endereco) {
+
+        if (nome == null || nome.isEmpty()) {
+            System.out.println("Nome obrigatório");
+            nome = "Não foi informado";
+        }
+
+        if (email == null || !email.contains("@")) {
+            System.out.println("Email inválido");
+            email = "email@invalido.com";
+        }
+
         this.id = id;
         this.nome = nome;
         this.telefone = telefone;
@@ -15,6 +27,9 @@ public class Usuario {
         this.endereco = endereco;
     }
 
-    public int getId() { return id; }
-    public String getNome() { return nome; }
+    public int getId() {return id;}
+    public String getNome() {return nome;}
+    public String getTelefone() {return telefone;}
+    public String getEmail() {return email;}
+    public String getEndereco() {return endereco;}
 }
