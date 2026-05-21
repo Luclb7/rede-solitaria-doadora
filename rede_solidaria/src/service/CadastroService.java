7bda8f8b798c95daa1485fe879eca4e9f6bfce6f
gilleanmeneses <gilleanmenesesfarias1@gmail.com>
2026-05-21 16:52:32 -0300

    Add success messages for cadastro methods

--- rede_solidaria/src/service/CadastroService.java
+++ rede_solidaria/src/service/CadastroService.java
@@ -7,13 +7,16 @@ public class CadastroService {
 
     public void cadastrarDoador(Doador d) {
         BancoDados.doadores.add(d);
+        System.out.println("Doador cadastrado com sucesso.");
     }
 
     public void cadastrarBeneficiario(Beneficiario b) {
         BancoDados.beneficiarios.add(b);
+        System.out.println("Beneficiário cadastrado com sucesso.");
     }
 
     public void cadastrarItem(ItemDoacao i) {
         BancoDados.itens.add(i);
+        System.out.println("Item cadastrado com sucesso.");
     }
 }
